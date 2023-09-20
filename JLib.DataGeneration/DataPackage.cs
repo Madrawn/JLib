f8dd69344de6b29a637d6914d360298ae11bb7da
Marcel Rehkemper <rehkemper@busitec.de>
2023-09-20 08:29:19 +0200

    Data Packages work

--- JLib.DataGeneration/DataPackage.cs
+++ JLib.DataGeneration/DataPackage.cs
@@ -1,15 +1,44 @@
-﻿using JLib.Data;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using JLib.Data;
+using JLib.Helper;
 using static JLib.FactoryAttributes.TvtFactoryAttributes;
 
 namespace JLib.DataGeneration;
 
-[IsDerivedFromAny<DataPackage<IEntity>>, NotAbstract]
+[IsDerivedFrom<DataPackage>, NotAbstract]
 public record DataPackageType(Type Value) : TypeValueType(Value);
 
-public abstract class DataPackage<TEntity> where TEntity : IEntity
+public abstract class DataPackage
 {
-    public DataPackage()
+    readonly IDataPackageStore _dataPackages;
+    protected DataPackage(IDataPackageStore dataPackages)
     {
-        //typeof(DataPackage<>).
+        _dataPackages = dataPackages;
+        foreach (var propertyInfo in GetType().GetProperties())
+        {
+            if (!propertyInfo.PropertyType.IsAssignableTo<GuidValueType>())
+                continue;
+            if (propertyInfo.GetMethod?.IsPublic is not true)
+                continue;
+            if (propertyInfo.CanWrite is false)
+                throw new(propertyInfo.DeclaringType?.FullClassName() + "." + propertyInfo.Name +
+                          " can not be written");
+            if (propertyInfo.SetMethod?.IsPublic is true)
+                throw new(propertyInfo.DeclaringType?.FullClassName() + "." + propertyInfo.Name + 
+                          " set method must be protected");
+            var id = _dataPackages.RetrieveId(propertyInfo);
+            propertyInfo.SetValue(this, id);
+        }
     }
+
+    protected TEntity[] AddEntities<TEntity>(IEnumerable<TEntity> entities)
+        where TEntity : IEntity
+        => _dataPackages.AddEntities(entities);
+
+
+    protected TId? DeriveId<TId>(GuidValueType? id)
+        where TId : GuidValueType
+        => _dataPackages.DeriveId<TId>(id, GetType());
+
 }
