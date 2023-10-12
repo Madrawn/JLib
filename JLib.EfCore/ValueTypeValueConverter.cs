2f63da3a8956bda92eff09fd01424ab600b27d89
Marcel Rehkemper <rehkemper@busitec.de>
2023-10-12 15:45:08 +0200

    fixed nullablillity valuetypeconverter

--- JLib.EfCore/ValueTypeValueConverter.cs
+++ JLib.EfCore/ValueTypeValueConverter.cs
@@ -8,48 +8,62 @@ using AutoMapper;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace JLib.EfCore;
-public class ClassValueTypeValueConverter<TValueType, TValue> : ValueConverter<TValueType, TValue?>
+public class ClassValueTypeValueConverter<TValueType, TValue> : ValueConverter<TValueType?, TValue?>
     where TValueType : ValueType<TValue>
     where TValue : class
 {
-    private static Expression<Func<TValue?, TValueType>> GetVtFactory()
+    private static Expression<Func<TValue?, TValueType?>> GetVtFactory()
     {
         var vtType = typeof(TValueType);
         var par = Expression.Parameter(typeof(TValue), "value");
         var ctor = vtType.GetConstructor(new[] { typeof(TValue) }) ?? throw new Exception("ctor not found");
         var ctorEx = Expression.New(ctor, par);
-        var nullValue = Expression.Constant(null, typeof(TValue));
-        var compare = Expression.Equal(par, nullValue);
-        var ifEx = Expression.IfThenElse(
+        var vNullValue = Expression.Constant(null, typeof(TValue));
+        var tvtNullValue = Expression.Constant(null, typeof(TValueType));
+        var compare = Expression.Equal(par, vNullValue);
+        var returnEx = Expression.Label(typeof(TValueType));
+        var ifEx = Expression.IfThen(
             compare,
-            Expression.Constant(null, typeof(TValue)),
-            ctorEx);
-        return Expression.Lambda<Func<TValue?, TValueType>>(ifEx, par);
+            Expression.Return(returnEx, tvtNullValue));
+
+        var body = Expression.Block(
+            ifEx,
+            Expression.Label(returnEx, ctorEx)
+        );
+
+        return Expression.Lambda<Func<TValue?, TValueType?>>(body, par);
     }
-    public ClassValueTypeValueConverter() : base(x => x.Value, GetVtFactory())
+    public ClassValueTypeValueConverter() : base(x => x == null ? null : x.Value, GetVtFactory())
     {
 
     }
 }
-public class StructValueTypeValueConverter<TValueType, TValue> : ValueConverter<TValueType, TValue?>
+public class StructValueTypeValueConverter<TValueType, TValue> : ValueConverter<TValueType?, TValue?>
     where TValueType : ValueType<TValue>
     where TValue : struct
 {
-    private static Expression<Func<TValue?, TValueType>> GetVtFactory()
+    private static Expression<Func<TValue?, TValueType?>> GetVtFactory()
     {
         var vtType = typeof(TValueType);
         var par = Expression.Parameter(typeof(TValue), "value");
         var ctor = vtType.GetConstructor(new[] { typeof(TValue) }) ?? throw new Exception("ctor not found");
         var ctorEx = Expression.New(ctor, par);
-        var nullValue = Expression.Constant(null, typeof(TValue?));
-        var compare = Expression.Equal(par, nullValue);
-        var ifEx = Expression.IfThenElse(
+        var vNullValue = Expression.Constant(null, typeof(TValue));
+        var tvtNullValue = Expression.Constant(null, typeof(TValueType));
+        var compare = Expression.Equal(par, vNullValue);
+        var returnEx = Expression.Label(typeof(TValueType));
+        var ifEx = Expression.IfThen(
             compare,
-            Expression.Constant(null, typeof(TValue)),
-            ctorEx);
-        return Expression.Lambda<Func<TValue?, TValueType>>(ifEx, par);
+            Expression.Return(returnEx, tvtNullValue));
+
+        var body = Expression.Block(
+            ifEx,
+            Expression.Label(returnEx, ctorEx)
+        );
+
+        return Expression.Lambda<Func<TValue?, TValueType?>>(body, par);
     }
-    public StructValueTypeValueConverter() : base(x => x.Value, GetVtFactory())
+    public StructValueTypeValueConverter() : base(x => x == null ? null : x.Value, GetVtFactory())
     {
 
     }
