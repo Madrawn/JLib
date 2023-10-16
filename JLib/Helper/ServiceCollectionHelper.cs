ee66cd00fcb259f49fbc59c096f77619dc0409d3
Marcel Rehkemper <rehkemper@busitec.de>
2023-10-16 11:56:19 +0200

    working on automated Automapper profile generation

--- JLib/Helper/ServiceCollectionHelper.cs
+++ JLib/Helper/ServiceCollectionHelper.cs
@@ -180,7 +180,7 @@ public static class ServiceCollectionHelper
         {
             foreach (var typeMappingInfoEx in mappedDataObjectType.MappingInfo)
             {
-                var (sourceType, destinationType, providerMode, _) =
+                var (sourceType, destinationType, providerMode, _, _) =
                     typeMappingInfoEx;
 
                 var repo = typeCache.TryGet<RepositoryType>(
