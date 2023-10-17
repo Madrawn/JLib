827202c305d1f1c1e2254d70de6b9b34e01ebcf0
Marcel Rehkemper <rehkemper@busitec.de>
2023-10-17 14:51:10 +0200

    DiagnosticsLogger added

---
+++ JLib.HotChocolate/DiagnosticsObserver.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DiagnosticAdapter;
+using Microsoft.Extensions.Logging;
+using ILogger = Serilog.ILogger;
+using HotChocolate.Diagnostics;
+using HotChocolate.Execution.Instrumentation;
+using HotChocolate.Execution;
+using HotChocolate.Execution.Processing;
+using Serilog;
+
+namespace JLib.HotChocolate;
+public class LoggingEventListener : ExecutionDiagnosticEventListener
+{
+    private readonly ILogger<LoggingEventListener> _logger;
+
+    public LoggingEventListener(ILogger<LoggingEventListener> logger)
+        => _logger = logger;
+
+    public override IDisposable ExecuteRequest(IRequestContext context)
+    {
+        _logger.LogDebug("Executing Request");
+        _logger.LogTrace("request data: {requestData}", context);
+        var disposable = base.ExecuteRequest(context);
+        _logger.LogTrace("request completed");
+        return disposable;
+    }
+
+    public override IDisposable ExecuteOperation(IRequestContext context)
+    {
+        _logger.LogDebug("Executing Operation");
+        _logger.LogTrace("request data: {requestData}", context);
+        var disposable = base.ExecuteOperation(context);
+        _logger.LogTrace("request completed");
+        return disposable;
+    }
+
+    public override IDisposable ExecuteSubscription(ISubscription subscription)
+    {
+        _logger.LogDebug("Executing Subscription");
+        _logger.LogTrace("request data: {requestData}", subscription);
+        var disposable = base.ExecuteSubscription(subscription);
+        _logger.LogTrace("request completed");
+        return disposable;
+    }
+
+    public override void RequestError(IRequestContext context,
+        Exception exception)
+    {
+        _logger.LogError(exception, "A request error occurred!: {e}", exception.Message);
+    }
+}
