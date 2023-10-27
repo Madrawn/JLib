e965e7d0466a9aaee91afc487d3ccd7ccd87f14d
Marcel Rehkemper <rehkemper@busitec.de>
2023-10-27 09:09:59 +0200

    added a authorization overload which only requires a single func for authorization

--- JLib/Data/Authorization/AuthorizationProfile.cs
+++ JLib/Data/Authorization/AuthorizationProfile.cs
@@ -31,15 +31,72 @@ public abstract class AuthorizationProfile
     }
     #region AddAuthorization
 
+    /// <summary>
+    /// adds a authorization rule for all <typeparamref name="TDataObject"/>s
+    /// <br/>the <paramref name="queryPredicate"/> and <paramref name="dataObjectPredicate"/> are applied as is
+    /// </summary>
+    /// <typeparam name="TDataObject">the dataObject to be authorized</typeparam>
+    /// <typeparam name="TS1">the first service</typeparam>
+    /// <param name="queryPredicate">used to authorize queries</param>
+    /// <param name="dataObjectPredicate">used to authorize materialized dataObjects</param>
     protected void AddAuthorization<TDataObject, TS1>(
-        Func<TS1, Expression<Func<TDataObject, bool>>> authorizationQueryablePredicate,
-        Func<TS1, TDataObject, bool> authorizationPredicate
+        Func<TS1, Expression<Func<TDataObject, bool>>> queryPredicate,
+        Func<TS1, TDataObject, bool> dataObjectPredicate
     )
         where TDataObject : class, IDataObject
         where TS1 : notnull
-        => _authorizationProvider.Add(new UnboundAuthorizationInfo<TDataObject, TS1>(authorizationQueryablePredicate, authorizationPredicate, _typeCache));
+        => _authorizationProvider.Add(new UnboundAuthorizationInfo<TDataObject, TS1>(queryPredicate, dataObjectPredicate, _typeCache));
+    /// <summary>
+    /// adds a authorization rule for all <typeparamref name="TDataObject"/>
+    /// the <paramref name="predicate"/> will be
+    /// - once compiled and used for data object Authorization
+    /// - visited so that the service parameter becomes a constant. this happens each time a query is authorized
+    /// <br/>
+    /// </summary>
+    /// <typeparam name="TDataObject">the dataObject to be authorized</typeparam>
+    /// <typeparam name="TS1">the first service</typeparam>
+    /// <param name="predicate">the authorization rule for this dataObject</param>
+    protected void AddAuthorization<TDataObject, TS1>(
+        Expression<Func<TS1, TDataObject, bool>> predicate
+    )
+        where TDataObject : class, IDataObject
+        where TS1 : notnull
+    {
+        var p1 = predicate.Parameters[0];
+        var p2 = predicate.Parameters[1];
+
+
+        _authorizationProvider.Add(new UnboundAuthorizationInfo<TDataObject, TS1>(ExpressionGenerator,
+            predicate.Compile(), _typeCache));
+
+        return;
+
+        Expression<Func<TDataObject, bool>> ExpressionGenerator(TS1 service)
+        {
+            var c = Expression.Constant(service, typeof(TS1));
+            var body = new ParToConstVisitor(p1, c).Visit(predicate.Body);
+            return Expression.Lambda<Func<TDataObject, bool>>(body, p2);
+        }
+    }
 
     #endregion
 
 
+}
+internal class ParToConstVisitor : ExpressionVisitor
+{
+    private readonly ParameterExpression _par;
+    private readonly ConstantExpression _const;
+
+    public ParToConstVisitor(ParameterExpression par, ConstantExpression @const)
+    {
+        _par = par;
+        _const = @const;
+    }
+    protected override Expression VisitParameter(ParameterExpression node)
+    {
+        return node == _par
+            ? _const
+            : base.VisitParameter(node);
+    }
 }
