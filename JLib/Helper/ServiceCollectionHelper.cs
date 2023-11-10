884b5d92f2785cc711fce3c57a15055efb4daf69
Marcel Rehkemper <rehkemper@busitec.de>
2023-11-10 11:59:27 +0100

    typo fixed

--- JLib/Helper/ServiceCollectionHelper.cs
+++ JLib/Helper/ServiceCollectionHelper.cs
@@ -102,7 +102,7 @@ public static class ServiceCollectionHelper
                 services,sectionInstance
             });
 
-            // extract value from options and myke section directly accessible
+            // extract value from options and make the section directly accessible
             var src = typeof(IOptions<>).MakeGenericType(sectionType);
             var prop = src.GetProperty(nameof(IOptions<Ignored>.Value)) ?? throw new InvalidSetupException("Value Prop not found on options");
             services.Add(
