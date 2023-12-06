dc8421c30afd22c6ca835a93bcdff3e9c24ca783
Marcel Rehkemper <rehkemper@busitec.de>
2023-12-06 16:46:01 +0100

    testing style

--- JLib.Tests/Reflection/TypeCacheTests.cs
+++ JLib.Tests/Reflection/TypeCacheTests.cs
@@ -13,36 +13,39 @@ using Snapshooter;
 using Snapshooter.Xunit;
 
 namespace JLib.Tests.Reflection;
+/// <summary>
+/// tests for multiple constellations of inputTypes for the typeCache<br/>
+/// to add a testcase, create a subclass with the expected types.<br/>
+/// the result will be validated via snapshot<br/>
+/// to run only a single test for debug purposes, add the <see cref="FocusTestAttribute"/> to the container class
+/// </summary>
 public class TypeCacheTests
 {
-    #region Test Environments
+
+    public static class TypeValueTypeWithoutFactoryAttribute
+    {
+        public record InvalidTypeValueType(Type Value) : TypeValueType(Value);
+    }
+    public static class InconclusiveFactory
+    {
+        public interface IDemoTypeA { }
+        public interface IDemoTypeB { }
+
+        [TvtFactoryAttributes.Implements(typeof(IDemoTypeA))]
+        public record DemoTypeValueTypeA(Type Value) : TypeValueType(Value);
+        [TvtFactoryAttributes.Implements(typeof(IDemoTypeB))]
+        public record DemoTypeValueTypeB(Type Value) : TypeValueType(Value);
+        public class InconclusiveType : IDemoTypeA, IDemoTypeB { }
+    }
+    
+
+    #region testing logic
     /// <summary>
     /// a parent class with this attribute will be the only test executed and cause the <see cref="TypeCacheTests.FocusNotSet"/> test to fail
     /// </summary>
     [AttributeUsage(AttributeTargets.Class)]
     // ReSharper disable once ClassNeverInstantiated.Local
     private class FocusTestAttribute : Attribute { }
-
-    public class TypeValueTypeWithoutFactoryAttribute
-    {
-        public record InvalidTypeValueType(Type Value) : TypeValueType(Value);
-    }
-
-    public class InconclusiveFactory
-    {
-        public interface IDemoTypeA { }
-        public interface IDemoTypeB { }
-        [TvtFactoryAttributes.Implements(typeof(IDemoTypeA))]
-        public record DemoTypeValueTypeA { }
-        [TvtFactoryAttributes.Implements(typeof(IDemoTypeB))]
-        public record DemoTypeValueTypeB { }
-        public class InconclusiveType : IDemoTypeA, IDemoTypeB { }
-    }
-    #endregion
-    public TypeCacheTests()
-    {
-    }
-
     public static object[][] GetTestTypes()
     {
         var anyFocus = typeof(TypeCacheTests).GetNestedTypes().Any(t => t.HasCustomAttribute<FocusTestAttribute>());
@@ -74,9 +77,9 @@ public class TypeCacheTests
                             sut.GetNestedTypes().ToDictionary(t=>t.Name,
                                 t => new Dictionary<string, object?>()
                                     {
-                                        {"Kind",t.IsClass?"Class":t.IsValueType?"struct":t.IsInterface?"Interface":"other"},
+                                        { "Kind", t.IsClass?"Class":t.IsValueType?"struct":t.IsInterface?"Interface":"other"},
                                         { "Attributes", t.GetCustomAttributes<Attribute>().Select(a=>a.GetType().FullClassName()) },
-                                        {"ImplementedInterfaces",t.GetInterfaces().Select(t2=>t2.FullClassName())},
+                                        { "ImplementedInterfaces", t.GetInterfaces().Select(t2=>t2.FullClassName())},
                                     }
                                 )
                         }
@@ -105,4 +108,5 @@ public class TypeCacheTests
             .Should()
             .OnlyContain(t => !t.HasCustomAttribute<FocusTestAttribute>(true), "Some tests have the Focus attribute set");
     }
+    #endregion
 }
