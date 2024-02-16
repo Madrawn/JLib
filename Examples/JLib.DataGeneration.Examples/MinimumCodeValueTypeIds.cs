779c7b87d2d5bc391038d858fbcb4b5e3a68cd53
Marcel Rehkemper <rehkemper@busitec.de>
2024-02-16 10:21:07 +0100

    datapackage examples rework

---
+++ Examples/JLib.DataGeneration.Examples/MinimumCodeValueTypeIds.cs
@@ -0,0 +1,74 @@
+﻿// Third party packages
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
+using Snapshooter.Xunit;
+using Xunit;
+
+// required JLib packages
+using JLib.DependencyInjection;
+using JLib.Exceptions;
+using JLib.Helper;
+using JLib.Reflection;
+using JLib.AutoMapper;
+
+// referenced example setup code
+using JLib.DataGeneration.Examples.Setup.Models;
+using JLib.DataGeneration.Examples.Setup.SystemUnderTest;
+
+namespace JLib.DataGeneration.Examples;
+public sealed class MinimumCodeValueTypeIds : IDisposable
+{
+    /*************************************************************\
+    |                       Data Packages                         |
+    \*************************************************************/
+    public class CustomerDp : DataPackage
+    {
+        public CustomerId CustomerId { get; set; } = null!;
+        public CustomerDp(ShoppingServiceMock shoppingService, IDataPackageManager packageManager) : base(packageManager)
+        {
+            shoppingService.AddCustomer(new(GetInfoText(nameof(CustomerId)))
+            {
+                Id = CustomerId
+            });
+        }
+    }
+
+    /*************************************************************\
+    |                           Setup                             |
+    \*************************************************************/
+    private readonly List<IDisposable> _disposables = new();
+    private readonly ShoppingServiceMock _shoppingService;
+
+    public MinimumCodeValueTypeIds(ITestOutputHelper testOutputHelper)
+    {
+        var exceptions = ExceptionBuilder.Create("setup");
+        var loggerFactory = new LoggerFactory().AddXunit(testOutputHelper);
+        
+        var serviceCollection = new ServiceCollection()
+            .AddTypeCache(out var typeCache, exceptions, loggerFactory,
+                JLibDataGenerationTp.Instance,
+                TypePackage.GetNested<MinimumCodeValueTypeIds>())
+            .AddSingleton<ShoppingServiceMock>()
+            .AddScopedAlias<IShoppingService, ShoppingServiceMock>()
+            .AddAutoMapper(b => b.AddProfiles(typeCache, loggerFactory))
+            .AddDataPackages(typeCache);
+
+        var serviceProvider = serviceCollection
+            .BuildServiceProvider()
+            .DisposeWith(_disposables)
+            .IncludeDataPackages<CustomerDp>();
+
+        _shoppingService = serviceProvider.GetRequiredService<ShoppingServiceMock>();
+    }
+    public void Dispose()
+        => _disposables.DisposeAll();
+
+
+    /*************************************************************\
+    |                            Test                             |
+    \*************************************************************/
+    [Fact]
+    public void Test()
+        => _shoppingService.Customers.MatchSnapshot();
+}
--- JLib.Helper/DisposableHelper.cs
+++ JLib.Helper/DisposableHelper.cs
@@ -1,6 +1,10 @@
 ﻿namespace JLib.Helper;
 public static class DisposableHelper
 {
+    /// <summary>
+    /// adds the <paramref name="disposable"/> to the <paramref name="disposables"/> list and returns the <paramref name="disposable"/>
+    /// </summary>
+    /// <returns>the <paramref name="disposable"/> argument</returns>
     public static T DisposeWith<T>(this T disposable, IList<IDisposable> disposables)
         where T:IDisposable
     {
@@ -8,6 +12,9 @@ public static class DisposableHelper
         return disposable;
     }
 
+    /// <summary>
+    /// Disposes all <paramref name="disposables"/>
+    /// </summary>
     public static void DisposeAll(this IEnumerable<IDisposable> disposables)
     {
         foreach (var disposable in disposables)
