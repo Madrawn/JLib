2cc6258fb5b3c17b065c9d0d9b2522fb2b48beeb
Marcel Rehkemper <rehkemper@busitec.de>
2024-02-28 15:05:52 +0100

    missing typePackageSuffix added

--- JLib.ValueTypes.Mapping/JLibConfigurationTp.cs
+++ JLib.ValueTypes.Mapping/JLibConfigurationTp.cs
@@ -3,7 +3,7 @@ using JLib.Reflection;
 
 namespace JLib.ValueTypes.Mapping;
 [TypePackageProvider]
-public static class JLibValueTypesMapping
+public static class JLibValueTypesMappingTp
 {
-    public static ITypePackage Instance { get; } = TypePackage.Get(typeof(JLibValueTypesMapping).Assembly);
+    public static ITypePackage Instance { get; } = TypePackage.Get(typeof(JLibValueTypesMappingTp).Assembly);
 }
