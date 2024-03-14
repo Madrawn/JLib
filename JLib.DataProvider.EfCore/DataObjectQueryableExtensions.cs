ceaaedd175ea549176eedeac075b3af31ca73f3a
Marcel Rehkemper <rehkemper@busitec.de>
2024-03-14 14:02:50 +0100

    feat: sume features added

---
+++ JLib.DataProvider.EfCore/DataObjectQueryableExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace JLib.DataProvider.EfCore;
+/// <summary>
+/// Provides extension methods for queryable collections of data objects.
+/// </summary>
+public static class DataObjectQueryableExtensions
+{
+    /// <summary>
+    /// Converts the queryable collection to a dictionary asynchronously, using the <see cref="IDataObject.Id"/> as the key.
+    /// </summary>
+    /// <typeparam name="TValue">The type of the values in the dictionary.</typeparam>
+    /// <param name="queryable">The queryable collection.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A dictionary with the values mapped by their IDs.</returns>
+    public static async Task<IReadOnlyDictionary<Guid, TValue>> ToDictionaryAsync<TValue>(this IQueryable<TValue> queryable, CancellationToken cancellationToken)
+        where TValue : IDataObject
+    {
+        return await queryable.ToDictionaryAsync(x => x.Id, cancellationToken);
+    }
+}
---
+++ JLib.DataProvider/Authorization/FullAccessAuthorizationInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq.Expressions;
+using JLib.Reflection;
+
+namespace JLib.DataProvider.Authorization;
+
+/// <summary>
+/// Represents the authorization information for full access to a specific data object.
+/// </summary>
+/// <typeparam name="TDataObject">The type of the data object.</typeparam>
+public class FullAccessAuthorizationInfo<TDataObject> : IAuthorizationInfo<TDataObject>
+    where TDataObject : class, IDataObject
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FullAccessAuthorizationInfo{TDataObject}"/> class.
+    /// </summary>
+    /// <param name="typeCache">The type cache.</param>
+    public FullAccessAuthorizationInfo(ITypeCache typeCache)
+    {
+        Target = typeCache.Get<DataObjectType>(typeof(TDataObject));
+    }
+
+    /// <summary>
+    /// Gets the target data object type.
+    /// </summary>
+    public DataObjectType Target { get; }
+
+    /// <summary>
+    /// Gets the expression representing the authorization condition.
+    /// </summary>
+    /// <returns>The expression representing the authorization condition.</returns>
+    public Expression<Func<TDataObject, bool>> Expression() => _ => true;
+
+    /// <summary>
+    /// Checks if the specified data object is authorized.
+    /// </summary>
+    /// <param name="dataObject">The data object to check.</param>
+    /// <returns><c>true</c> if the data object is authorized; otherwise, <c>false</c>.</returns>
+    public bool DataObject(TDataObject dataObject) => true;
+}
