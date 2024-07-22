3c1443a02b49d6e654a9798bb288597893ee7604
Marcel Rehkemper <rehkemper@busitec.de>
2024-07-22 16:25:07 +0200

    int validation context extensions improved

--- JLib.ValueTypes/ValidationContextHelper/IntValidationContextExtensions.cs
+++ JLib.ValueTypes/ValidationContextHelper/IntValidationContextExtensions.cs
@@ -9,11 +9,11 @@ public static class IntValidationContextExtensions
     /// Validates that the value is greater than or equal to the specified minimum.
     /// </summary>
     /// <param name="minimum">The minimum value.</param>
-    /// <returns>The current instance of the <see cref="IntValidator"/> class.</returns>
-    public static ValidationContext<int> BeGreaterOrEqualThan(this ValidationContext<int> context,int minimum)
+    /// <returns>The current instance of the <see cref="ValidationContext{T}"/> class.</returns>
+    public static ValidationContext<int> BeGreaterThanOrEqualTo(this ValidationContext<int> context,int minimum)
     {
         if (context.Value < minimum)
-            context.AddError($"value is smaller than {minimum}");
+            context.AddError($"value must be greater than or equal to {minimum}");
         return context;
     }
 
@@ -21,11 +21,11 @@ public static class IntValidationContextExtensions
     /// Validates that the value is less than or equal to the specified maximum.
     /// </summary>
     /// <param name="maximum">The maximum value.</param>
-    /// <returns>The current instance of the <see cref="IntValidator"/> class.</returns>
-    public static ValidationContext<int> BeLessOrEqualThan(this ValidationContext<int> context, int maximum)
+    /// <returns>The current instance of the <see cref="ValidationContext{T}"/> class.</returns>
+    public static ValidationContext<int> BeLessThanOrEqualTo(this ValidationContext<int> context, int maximum)
     {
         if (context.Value > maximum)
-            context.AddError($"value is greater than {maximum}");
+            context.AddError($"value must be less than or equal to {maximum}");
         return context;
     }
 
@@ -33,11 +33,11 @@ public static class IntValidationContextExtensions
     /// Validates that the value is greater than the specified minimum.
     /// </summary>
     /// <param name="minimum">The minimum value.</param>
-    /// <returns>The current instance of the <see cref="IntValidator"/> class.</returns>
+    /// <returns>The current instance of the <see cref="ValidationContext{T}"/> class.</returns>
     public static ValidationContext<int> BeGreaterThan(this ValidationContext<int> context, int minimum)
     {
         if (context.Value <= minimum)
-            context.AddError($"value is smaller than or equal to {minimum}");
+            context.AddError($"value must be greater than {minimum}");
         return context;
     }
 
@@ -45,35 +45,36 @@ public static class IntValidationContextExtensions
     /// Validates that the value is less than the specified maximum.
     /// </summary>
     /// <param name="maximum">The maximum value.</param>
-    /// <returns>The current instance of the <see cref="IntValidator"/> class.</returns>
+    /// <returns>The current instance of the <see cref="ValidationContext{T}"/> class.</returns>
     public static ValidationContext<int> BeLessThan(this ValidationContext<int> context, int maximum)
     {
         if (context.Value >= maximum)
-            context.AddError($"value is greater than or equal to {maximum}");
+            context.AddError($"value must be less than {maximum}");
         return context;
     }
 
     /// <summary>
     /// Validates that the value is not negative (greater than or equal to 0).
     /// </summary>
-    /// <returns>The current instance of the <see cref="IntValidator"/> class.</returns>
+    /// <returns>The current instance of the <see cref="ValidationContext{T}"/> class.</returns>
     public static ValidationContext<int> NotBeNegative(this ValidationContext<int> context)
-        => context.BeGreaterOrEqualThan(0);
+        => context.BeGreaterThanOrEqualTo(0);
 
     /// <summary>
     /// Validates that the value is positive (greater than 0).
     /// </summary>
-    /// <returns>The current instance of the <see cref="IntValidator"/> class.</returns>
+    /// <returns>The current instance of the <see cref="ValidationContext{T}"/> class.</returns>
     public static ValidationContext<int> BePositive(this ValidationContext<int> context)
         => context.BeGreaterThan(0);
 
     /// <summary>
     /// Validates that the value is within the specified range (between and including the minimum and maximum).
     /// </summary>
+    /// <param name="context">The current Validation Context</param>
     /// <param name="minimum">The minimum value.</param>
     /// <param name="maximum">The maximum value.</param>
-    /// <returns>The current instance of the <see cref="IntValidator"/> class.</returns>
+    /// <returns>The current instance of the <see cref="ValidationContext{T}"/> class.</returns>
     public static ValidationContext<int> BeInBounds(this ValidationContext<int> context, int minimum, int maximum)
-        => context.BeGreaterOrEqualThan(minimum)
-            .BeLessOrEqualThan(maximum);
+        => context.BeGreaterThanOrEqualTo(minimum)
+            .BeLessThanOrEqualTo(maximum);
 }
