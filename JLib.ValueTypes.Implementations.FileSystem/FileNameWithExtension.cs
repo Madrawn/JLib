851ebfcae8d6c6d933abfadba3c65b7d0eb95ed8
Marcel Rehkemper <rehkemper@busitec.de>
2024-08-01 09:23:32 +0200

    JLib.ValueTypes.Implementations.FileSystem added

---
+++ JLib.ValueTypes.Implementations.FileSystem/FileNameWithExtension.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JLib.ValueTypes.Definitions.FileSystem;
+
+public interface IPathSegment;
+public interface IPath;
+
+public record FileNameWithExtension(string Value) : StringValueType(Value), IPathSegment
+{
+    [Validation]
+    private static void Validate(ValidationContext<string> must)
+        => must
+            .NotContain(Path.DirectorySeparatorChar)
+            .NotContain(Path.AltDirectorySeparatorChar)
+            .Contain('.');
+}
+public record FileNameWithoutExtension(string Value) : StringValueType(Value)
+{
+    [Validation]
+    private static void Validate(ValidationContext<string> must)
+        => must
+            .NotContain(Path.DirectorySeparatorChar)
+            .NotContain(Path.AltDirectorySeparatorChar);
+
+    /// <summary>
+    /// appends the <paramref name="extension"/> to the <paramref name="name"/>. If the extension is empty, the name is returned as is.
+    /// </summary>
+    public static FileNameWithExtension operator +(FileNameWithoutExtension name, FileExtension extension)
+        => new(
+            extension.Value == "" 
+            ? name.Value 
+            : $"{name.Value}.{extension.Value}"
+        );
+}
+public record FileExtension(string Value) : StringValueType(Value)
+{
+    [Validation]
+    private static void Validate(ValidationContext<string> must)
+        => must
+            .NotStartWith('.')
+            .NotContain(Path.GetInvalidFileNameChars());
+}
+
+/// <summary>
+/// the name of a single directory in a path, not a <see cref="RelativeDirectoryPath"/>, <see cref="AbsoluteDirectoryPath"/> or <see cref="DriveLetter"/>
+/// </summary>
+/// <param name="Value"></param>
+public record DirectoryName(string Value) : StringValueType(Value)
+{
+    [Validation]
+    private static void Validate(ValidationContext<string> must)
+        => must
+            .NotContain(Path.GetInvalidPathChars())
+            .NotContain(Path.DirectorySeparatorChar)
+            .NotContain(Path.AltDirectorySeparatorChar);
+}
+public record RelativeDirectoryPath(string Value) : StringValueType(Value)
+{
+    [Validation]
+    private static void Validate(ValidationContext<string> must)
+        => must
+            .NotContain(':')
+            .NotContain(Path.DirectorySeparatorChar)
+            .NotContain(Path.AltDirectorySeparatorChar);
+}
+public record AbsoluteDirectoryPath(string Value) : StringValueType(Value)
+{
+    [Validation]
+    private static void Validate(ValidationContext<string> must)
+        => must
+            .NotContain(Path.DirectorySeparatorChar)
+            .NotContain(Path.AltDirectorySeparatorChar)
+            .BeAscii()
+            .BeAlphanumeric();
+    public static AbsoluteDirectoryPath operator +(AbsoluteDirectoryPath path, RelativeDirectoryPath relativePath)
+        => new(Path.Combine(path.Value, relativePath.Value));
+}
+public record DriveLetter(char Value) : CharValueType(Value)
+{
+    [Validation]
+    private static void Validate(ValidationContext<char> must)
+        => must.BeAsciiLetter();
+
+    public static AbsoluteDirectoryPath operator +(DriveLetter letter, RelativeDirectoryPath path)
+        => new($"{letter.Value}:{Path.DirectorySeparatorChar}{path.Value}");
+}
---
+++ JLib.ValueTypes/ValidationContextHelper/CharValidationContextExtensions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JLib.ValueTypes;
+/// <summary>
+/// Contains validation methods for ValidationContext&lt;char&gt;.
+/// </summary>
+/// <seealso cref="ValidationContext{TValue}"/>
+public static class CharValidationContextExtensions
+{
+    /// <summary>
+    /// Validates if the character is a digit.
+    /// </summary>
+    /// <param name="context">The validation context.</param>
+    /// <returns>The validation context.</returns>
+    public static ValidationContext<char> BeDigit(this ValidationContext<char> context)
+    {
+        if (!char.IsDigit(context.Value))
+            context.AddError("must be a digit");
+        return context;
+    }
+
+    /// <summary>
+    /// Validates if the character is a letter.
+    /// </summary>
+    /// <param name="context">The validation context.</param>
+    /// <returns>The validation context.</returns>
+    public static ValidationContext<char> BeLetter(this ValidationContext<char> context)
+    {
+        if (!char.IsLetter(context.Value))
+            context.AddError("must be a letter");
+        return context;
+    }
+    /// <summary>
+    /// Validates if the character is ascii.
+    /// </summary>
+    /// <param name="context">The validation context.</param>
+    /// <returns>The validation context.</returns>
+    public static ValidationContext<char> BeAscii(this ValidationContext<char> context)
+    {
+        if (!char.IsAscii(context.Value))
+            context.AddError("must be ascii");
+        return context;
+    }
+    /// <summary>
+    /// Validates if the character is ascii.
+    /// </summary>
+    /// <param name="context">The validation context.</param>
+    /// <returns>The validation context.</returns>
+    public static ValidationContext<char> BeAsciiLetter(this ValidationContext<char> context)
+    {
+        // this should not be used, since it is not supported by .net 6.
+        // a compiler switch should not be used, since this might introduce a breaking change between .net versions.
+        //   if (!char.IsAsciiLetter(context.Value))
+        //       context.AddError("must be an ascii letter");
+        context.BeAscii();
+        context.BeLetter();
+        return context;
+    }
+}
