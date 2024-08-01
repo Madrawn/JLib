d6ac1fe47e075b052a0d71df9e77bf7b440b6732
Marcel Rehkemper <rehkemper@busitec.de>
2024-08-01 10:25:54 +0200

    .net 6 compilation fixes and documentation

--- JLib.ValueTypes.Implementations.FileSystem/FileNameWithExtension.cs
+++ JLib.ValueTypes.Implementations.FileSystem/FileNameWithExtension.cs
@@ -1,41 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace JLib.ValueTypes.Implementations.FileSystem;
 
-namespace JLib.ValueTypes.Definitions.FileSystem;
+/// <summary>
+/// Identifies, the implementing ValueType as part of a filepath
+/// </summary>
+public interface IPathSegment { }
 
-public interface IPathSegment;
-public interface IPath;
-
+/// <summary>
+/// any valid filename without path information, but with and file extension<br/>
+/// Validation may differ between operating systems due to the usage of <see cref="Path.GetInvalidFileNameChars"/><br/>
+/// must not contain <see cref="Path.GetInvalidFileNameChars"/><br/>
+/// must contain '.'
+/// </summary>
+/// <param name="Value">the filename</param>
 public record FileNameWithExtension(string Value) : StringValueType(Value), IPathSegment
 {
     [Validation]
     private static void Validate(ValidationContext<string> must)
         => must
-            .NotContain(Path.DirectorySeparatorChar)
-            .NotContain(Path.AltDirectorySeparatorChar)
+            .NotContain(Path.GetInvalidFileNameChars())
             .Contain('.');
 }
+/// <summary> 
+/// any valid filename with path information and file extension<br/>
+/// Validation may differ between operating systems due to the usage of <see cref="Path.GetInvalidFileNameChars"/><br/>
+/// must not contain <see cref="Path.GetInvalidFileNameChars"/><br/>
+/// </summary>
+/// <param name="Value">the filename</param>
 public record FileNameWithoutExtension(string Value) : StringValueType(Value)
 {
     [Validation]
     private static void Validate(ValidationContext<string> must)
         => must
-            .NotContain(Path.DirectorySeparatorChar)
-            .NotContain(Path.AltDirectorySeparatorChar);
+            .NotContain(Path.GetInvalidFileNameChars());
 
     /// <summary>
     /// appends the <paramref name="extension"/> to the <paramref name="name"/>. If the extension is empty, the name is returned as is.
     /// </summary>
     public static FileNameWithExtension operator +(FileNameWithoutExtension name, FileExtension extension)
         => new(
-            extension.Value == "" 
-            ? name.Value 
+            extension.Value == ""
+            ? name.Value
             : $"{name.Value}.{extension.Value}"
         );
 }
+
+/// <summary>
+/// any valid file extension<br/>
+/// Validation may differ between operating systems due to the usage of <see cref="Path.GetInvalidFileNameChars"/><br/>
+/// must not contain <see cref="Path.GetInvalidFileNameChars"/><br/>
+/// must not start with '.'
+/// </summary>
+/// <param name="Value">the file extension</param>
 public record FileExtension(string Value) : StringValueType(Value)
 {
     [Validation]
@@ -46,9 +61,13 @@ public record FileExtension(string Value) : StringValueType(Value)
 }
 
 /// <summary>
-/// the name of a single directory in a path, not a <see cref="RelativeDirectoryPath"/>, <see cref="AbsoluteDirectoryPath"/> or <see cref="DriveLetter"/>
+/// the name of a single directory in a path, not a <see cref="RelativeDirectoryPath"/>, <see cref="AbsoluteDirectoryPath"/> or <see cref="DriveLetter"/><br/><br/>
+/// Validation may differ between operating systems due to the usage of <see cref="Path.GetInvalidPathChars"/><br/><br/>
+/// must not contain <see cref="Path.GetInvalidPathChars"/><br/>
+/// must not contain <see cref="Path.DirectorySeparatorChar"/><br/>
+/// must not contain <see cref="Path.AltDirectorySeparatorChar"/><br/>
 /// </summary>
-/// <param name="Value"></param>
+/// <param name="Value">the directory name</param>
 public record DirectoryName(string Value) : StringValueType(Value)
 {
     [Validation]
@@ -58,14 +77,28 @@ public record DirectoryName(string Value) : StringValueType(Value)
             .NotContain(Path.DirectorySeparatorChar)
             .NotContain(Path.AltDirectorySeparatorChar);
 }
+
+/// <summary>
+/// a relative directory path <see cref="AbsoluteDirectoryPath"/>. It may contain only one directory and may use relative navigation<br/><br/>
+/// Validation may differ between operating systems due to the usage of <see cref="Path.GetInvalidPathChars"/>
+/// <remarks>
+/// <br/><br/>
+/// <see cref="Path.IsPathRooted(ReadOnlySpan{char})"/> must evaluate <paramref name="Value"/> to false
+/// must not contain <see cref="Path.GetInvalidPathChars"/><br/>
+/// may contain <see cref="Path.DirectorySeparatorChar"/><br/>
+/// may contain <see cref="Path.AltDirectorySeparatorChar"/><br/>
+/// </remarks>
+/// </summary>
+/// <param name="Value">the directory name</param>
 public record RelativeDirectoryPath(string Value) : StringValueType(Value)
 {
     [Validation]
-    private static void Validate(ValidationContext<string> must)
-        => must
-            .NotContain(':')
-            .NotContain(Path.DirectorySeparatorChar)
-            .NotContain(Path.AltDirectorySeparatorChar);
+    private static void Validate(ValidationContext<string> context)
+    {
+        if (Path.IsPathRooted(context.Value))
+            context.AddError("The path must not be rooted");
+        context.NotContain(Path.GetInvalidPathChars());
+    }
 }
 public record AbsoluteDirectoryPath(string Value) : StringValueType(Value)
 {
---
+++ JLib.ValueTypes.Implementations.FileSystem/JLib.ValueTypes.Implementations.FileSystem Documentation.md
@@ -0,0 +1,2 @@
+﻿contains Validated implementations of File System path segments like file names, directories, and file extensions. 
+ 
