7bc93bfbc796ee7c20e1e3bfd25afdda627f8cfd
Marcel Rehkemper <rehkemper@busitec.de>
2024-09-18 10:57:17 +0200

    AddTypeExtensions now works with base class type extensions

--- JLib.HotChocolate/Helper/RequestExecutorBuilderHelper.cs
+++ JLib.HotChocolate/Helper/RequestExecutorBuilderHelper.cs
@@ -10,9 +10,37 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace JLib.HotChocolate.Helper;
 
+/// <summary>
+/// a type which extends a given graphql object.
+/// </summary>
+/// <seealso cref="AttributeTypeExtensionType"/>
+/// <seealso cref="ClassTypeExtensionType"/>
+public abstract record TypeExtensionType : TypeValueType
+{
+    /// <summary>
+    /// <inheritdoc cref="TypeExtensionType"/>
+    /// </summary>
+    /// <param name="Value"></param>
+    internal TypeExtensionType(Type Value) : base(Value)
+    {
+    }
+}
+/// <summary>
+/// a type which extends a given graphql object.
+/// </summary>
 [TvtFactoryAttribute.IsClass, TvtFactoryAttribute.HasAttribute(typeof(ExtendObjectTypeAttribute))]
-public record TypeExtensionType(Type Value) : TypeValueType(Value);
+public record AttributeTypeExtensionType(Type Value) : TypeExtensionType(Value);
 
+
+/// <summary>
+/// a type which extends a given graphql object.
+/// </summary>
+[TvtFactoryAttribute.IsClass, TvtFactoryAttribute.IsDerivedFrom(typeof(ObjectTypeExtension))]
+public record ClassTypeExtensionType(Type Value) : TypeExtensionType(Value);
+
+/// <summary>
+/// extension methods for the <seealso cref="IRequestExecutorBuilder"/>
+/// </summary>
 public static class RequestExecutorBuilderHelper
 {
 
@@ -33,8 +61,8 @@ public static class RequestExecutorBuilderHelper
     public static IRequestExecutorBuilder RegisterDataProvider(this IRequestExecutorBuilder builder)
     {
         foreach (var dataProviderRType in builder.Services
-                     .Select(x=>x.ServiceType)
-                     .Where(x=>x.ImplementsAny<IDataProviderR<IDataObject>>())
+                     .Select(x => x.ServiceType)
+                     .Where(x => x.ImplementsAny<IDataProviderR<IDataObject>>())
                      .ToArray())
             builder.RegisterService(dataProviderRType);
         foreach (var dataProviderRwType in builder.Services
@@ -50,6 +78,16 @@ public static class RequestExecutorBuilderHelper
             .GetMethod(nameof(RequestExecutorBuilderExtensions.RegisterService))
         ?? throw new InvalidSetupException(
             $"{nameof(IRequestExecutorBuilder)}.{nameof(RequestExecutorBuilderExtensions.RegisterService)}Method Info not found");
+    /// <summary>
+    /// <inheritdoc cref="RequestExecutorBuilderExtensions.RegisterService{TService}"/>
+    /// </summary>
+    /// <param name="builder">The <see cref="IRequestExecutorBuilder"/>.</param>
+    /// <param name="service">The service type.</param>
+    /// <param name="serviceKind">The service kind defines the way a service is injected and handled by the execution engine.</param>
+    /// <returns>
+    /// An <see cref="IRequestExecutorBuilder"/> that can be used to configure a schema
+    /// and its execution.
+    /// </returns>
     public static IRequestExecutorBuilder RegisterService(
         this IRequestExecutorBuilder builder, Type service, ServiceKind serviceKind = ServiceKind.Default)
     {
