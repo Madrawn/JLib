df24d0ac1717acd4d27fdb7b15c8d8497159222c
Marcel Rehkemper <rehkemper@busitec.de>
2024-11-14 06:53:24 +0100

    propertyInfoValueType added

---
+++ JLib.Reflection/PropertyInfoValueType.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using JLib.ValueTypes;
+
+namespace JLib.Reflection;
+
+/// <summary>
+/// a <see cref="ValueType{T}"/> for <see cref="PropertyInfo"/>s
+/// </summary>
+/// <param name="Value"></param>
+[Unmapped]
+public abstract record PropertyInfoValueType(PropertyInfo Value) : ValueType<PropertyInfo>(Value)
+{
+    /// <summary>
+    /// <inheritdoc cref="ITypeValueType.Name"/>
+    /// </summary>
+    public string Name => Value.Name;
+}
