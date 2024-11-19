53ec13785a52fc6a4955fd416cae30e95e2c4b6e
Marcel Rehkemper <rehkemper@busitec.de>
2024-11-19 12:56:04 +0100

    documentation added

--- JLib.AspNetCore/AspNetCoreServiceCollectionExtensions.cs
+++ JLib.AspNetCore/AspNetCoreServiceCollectionExtensions.cs
@@ -10,22 +10,34 @@ using static JLib.AspNetCore.AspNetCoreServiceCollectionExtensions.AddRequestSco
 namespace JLib.AspNetCore;
 
 /// <summary>
-/// 
+/// <see cref="IServiceCollection"/> extension methods for asp.net core
 /// </summary>
 public static class AspNetCoreServiceCollectionExtensions
 {
-    public class AddRequestScopedServiceException : JLibException
+    /// <summary>
+    /// implies that something went wrong while adding a request scoped service or resolving the given <see cref="ServiceType"/>
+    /// </summary>
+    public abstract class AddRequestScopedServiceException : JLibException
     {
+        /// <summary>
+        /// the service type that could not be added
+        /// </summary>
         public Type ServiceType { get; }
+        /// <summary>
+        /// the implementation type that could not be added
+        /// </summary>
         public Type? ImplementationType { get; }
 
-        public AddRequestScopedServiceException(Type serviceType, Type? implementationType, string message, Exception? innerException) : base(message, innerException)
+        private protected AddRequestScopedServiceException(Type serviceType, Type? implementationType, string message, Exception? innerException) : base(message, innerException)
         {
             ServiceType = serviceType;
             Data[nameof(ServiceType)] = ServiceType;
             ImplementationType = implementationType;
             Data[nameof(ImplementationType)] = ImplementationType;
         }
+        /// <summary>
+        /// indicates, that something went after the <see cref="IServiceProvider"/> has been built
+        /// </summary>
         public abstract class RuntimeException : AddRequestScopedServiceException
         {
             private protected RuntimeException(Type serviceType, Type? implementationType, string message, Exception? innerException) : base(serviceType, implementationType,
@@ -37,6 +49,9 @@ public static class AspNetCoreServiceCollectionExtensions
             {
             }
         }
+        /// <summary>
+        /// indicates, that something went wrong before the <see cref="IServiceProvider"/> has been built
+        /// </summary>
         public abstract class InitializationException : AddRequestScopedServiceException
         {
             private protected InitializationException(Type serviceType, Type? implementationType, string message, Exception? innerException) : base(serviceType, implementationType,
@@ -49,24 +64,37 @@ public static class AspNetCoreServiceCollectionExtensions
             }
         }
 
+        /// <summary>
+        /// indicates, that the required service could not be found in the service collection
+        /// </summary>
         public class MissingRequirementException : RuntimeException
         {
-            public Type Requirement { get; }
+            /// <summary>
+            /// the required service that could not be found
+            /// </summary>
+            public Type RequiredService { get; }
 
-            internal MissingRequirementException(Type serviceType, Type? implementationType, Type requirement)
-                : base(serviceType, implementationType, $"Using Request Scoped Services requires the service {requirement} to be provided.", null)
+            internal MissingRequirementException(Type serviceType, Type? implementationType, Type requiredService)
+                : base(serviceType, implementationType, $"Using Request Scoped Services requires the service {requiredService} to be provided.", null)
             {
-                Requirement = requirement;
-                Data[nameof(Requirement)] = Requirement;
+                RequiredService = requiredService;
+                Data[nameof(RequiredService)] = RequiredService;
             }
         }
 
+        /// <summary>
+        /// indicates, that a scope without http context tried to inject the service.
+        /// this may happen when using the singleton scope or a scope manually created from it.
+        /// </summary>
         public class OutsideHttpContextScopeException : RuntimeException
         {
             internal OutsideHttpContextScopeException(Type serviceType, Type? implementationType) : base(serviceType, implementationType, "HttpContext is null", null)
             {
             }
         }
+        /// <summary>
+        /// indicates, that the service or implementation type is generic, which is not supported by this method.
+        /// </summary>
         public class UnsupportedGenericServiceException : InitializationException
         {
             internal UnsupportedGenericServiceException(Type serviceType, Type? implementationType) : base(serviceType, implementationType, "Generic Type Definitions are not supported yet.", null)
@@ -95,18 +123,30 @@ public static class AspNetCoreServiceCollectionExtensions
     /// <typeparam name="TService">the service to be provided</typeparam>
     /// <param name="services">the service collection to add the service to</param>
     /// <returns>the given <see cref="IServiceCollection"/></returns>
-    /// <exception cref="InvalidSetupException"></exception>
-    /// <exception cref="InvalidOperationException"></exception>
+    /// <exception cref="UnsupportedGenericServiceException"/>
+    /// <exception cref="MissingRequirementException"/>
+    /// <exception cref="OutsideHttpContextScopeException"/>
     public static IServiceCollection AddRequestScoped<TService>(this IServiceCollection services)
         where TService : class
         => services.AddRequestScoped<TService, TService>();
 
-    private static readonly MethodInfo GetRequiredServiceMi = typeof(ServiceProviderServiceExtensions)
-                                       .GetMethod(
-                                           "GetRequiredService",
-                                           1,
-                                           new[] { typeof(IServiceProvider) })
-                                   ?? throw new InvalidSetupException("IServiceProvider.GetRequiredService Method Info not found");
+    /// <summary>
+    /// provides the <paramref name="serviceType"/> once for each http request.
+    /// <br/>
+    /// <paramref name="serviceType"/> should be thread save, as scopes are often used to create new instances for each thread.
+    /// <remarks><br/>
+    /// This differs in comparison to a normal scoped service, in that when you create custom scopes for a request,
+    /// the service will be instanced multiple times when using a scoped service but only once using this method.
+    /// <br/>
+    /// this is useful for example when you have to make database requests to fetch a token based authentication info.
+    /// </remarks>
+    /// </summary>
+    /// <param name="services">the service collection to add the service to</param>
+    /// <param name="serviceType">the type of service to be provided</param>
+    /// <returns>the given <see cref="IServiceCollection"/></returns>
+    /// <exception cref="UnsupportedGenericServiceException"/>
+    /// <exception cref="MissingRequirementException"/>
+    /// <exception cref="OutsideHttpContextScopeException"/>
     public static IServiceCollection AddRequestScoped(this IServiceCollection services, Type serviceType)
         => services.AddRequestScoped(serviceType, serviceType);
 
@@ -126,8 +166,9 @@ public static class AspNetCoreServiceCollectionExtensions
     /// <typeparam name="TImplementation">the implementation to be used for <typeparam name="TService"></typeparam></typeparam>
     /// <param name="services">the service collection to add the service to</param>
     /// <returns>the given <see cref="IServiceCollection"/></returns>
-    /// <exception cref="InvalidSetupException"></exception>
-    /// <exception cref="InvalidOperationException"></exception>
+    /// <exception cref="UnsupportedGenericServiceException"/>
+    /// <exception cref="MissingRequirementException"/>
+    /// <exception cref="OutsideHttpContextScopeException"/>
     public static IServiceCollection AddRequestScoped<TService, TImplementation>(this IServiceCollection services)
         where TService : class
         where TImplementation : class, TService
@@ -150,8 +191,9 @@ public static class AspNetCoreServiceCollectionExtensions
     /// <param name="services">the service collection to add the service to</param>
     /// <param name="factory">the factory to create the service instance</param>
     /// <returns>the given <see cref="IServiceCollection"/></returns>
-    /// <exception cref="InvalidSetupException"></exception>
-    /// <exception cref="InvalidOperationException"></exception>
+    /// <exception cref="UnsupportedGenericServiceException"/>
+    /// <exception cref="MissingRequirementException"/>
+    /// <exception cref="OutsideHttpContextScopeException"/>
     public static IServiceCollection AddRequestScoped<TService>(this IServiceCollection services,
         Func<IServiceProvider, TService> factory)
         where TService : class
@@ -164,7 +206,24 @@ public static class AspNetCoreServiceCollectionExtensions
         => AddRequestScoped(services, factory, typeof(TService), implementationType);
 
 
-
+    /// <summary>
+    /// provides the <paramref name="implementationType"/> as <paramref name="serviceType"/> once for each http request.
+    /// <br/>
+    /// <paramref name="implementationType"/> should be thread save, as scopes are often used to create new instances for each thread.
+    /// <remarks><br/>
+    /// This differs in comparison to a normal scoped service, in that when you create custom scopes for a request,
+    /// the service will be instanced multiple times when using a scoped service but only once using this method.
+    /// <br/>
+    /// this is useful for example when you have to make database requests to fetch a token based authentication info.
+    /// </remarks>
+    /// </summary>
+    /// <param name="services">the service collection to add the service to</param>
+    /// <param name="serviceType">the type of service to be provided</param>
+    /// <param name="implementationType">the implementation used for <paramref name="serviceType"/></param>
+    /// <returns>the given <see cref="IServiceCollection"/></returns>
+    /// <exception cref="UnsupportedGenericServiceException"/>
+    /// <exception cref="MissingRequirementException"/>
+    /// <exception cref="OutsideHttpContextScopeException"/>
     public static IServiceCollection AddRequestScoped(this IServiceCollection services, Type serviceType, Type implementationType)
     {
         if (implementationType.IsGenericTypeDefinition)
@@ -176,7 +235,10 @@ public static class AspNetCoreServiceCollectionExtensions
 
         return services.AddRequestScoped(provider => factory.Invoke(provider, null), serviceType, implementationType);
     }
+    /// <param name="implementationType">only used for exception generation</param>
+#pragma warning disable CS1573 // Parameter has no matching param tag in the XML comment (but other parameters do)
     private static IServiceCollection AddRequestScoped(this IServiceCollection services, Func<IServiceProvider, object> factory, Type serviceType, Type? implementationType)
+#pragma warning restore CS1573 // Parameter has no matching param tag in the XML comment (but other parameters do)
     {
         services.TryAddScoped<ScopeIdGenerator>();
         services.AddScoped(serviceType, provider =>
