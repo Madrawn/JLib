9456959629f358b0ae0a2622bfd0f77cb3ec4947
Marcel Rehkemper <rehkemper@busitec.de>
2024-11-22 13:36:13 +0100

    IgnoredDataObject documented, IgnoredEntity added

--- JLib.DataProvider/ValueTypes/IgnoredDataObject.cs
+++ JLib.DataProvider/ValueTypes/IgnoredDataObject.cs
@@ -1,10 +1,30 @@
 ﻿namespace JLib.DataProvider;
 
+/// <summary>
+/// can be used as type argument if the given argument has to implement <see cref="IEntity"/> but is ignored due to reflection.
+/// </summary>
+public sealed class IgnoredEntity : IEntity
+{
+    private IgnoredEntity()
+    {
+    }
+
+    /// <summary>
+    /// <inheritdoc cref="IDataObject.Id"/>
+    /// </summary>
+    public Guid Id => default;
+}
+/// <summary>
+/// can be used as type argument if the given argument has to implement <see cref="IDataObject"/> but is ignored due to reflection.
+/// </summary>
 public sealed class IgnoredDataObject : IDataObject
 {
     private IgnoredDataObject()
     {
     }
 
-    public Guid Id { get; }
+    /// <summary>
+    /// <inheritdoc cref="IDataObject.Id"/>
+    /// </summary>
+    public Guid Id => default;
 }
