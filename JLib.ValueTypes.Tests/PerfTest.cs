91439b47e6ad203ab625c826dfef4da2f3a02218
Daniel Dengler <daniel@busitec.de>
2024-12-05 17:28:07 +0100

    Add PerfTest class and update test methods
    - Updated `GetRandom5LetterString` to generate a random 5-digit string.
    - Added `ProofOfConcept` test method to compare string concatenation methods.

--- JLib.ValueTypes.Tests/PerfTest.cs
+++ JLib.ValueTypes.Tests/PerfTest.cs
@@ -3,15 +3,19 @@
 using JLib.Exceptions;
 
 using Microsoft.VisualStudio.TestPlatform.Utilities;
+
 using System.Diagnostics;
 
 using Xunit;
 using Xunit.Abstractions;
 
 namespace JLib.ValueTypes.Tests;
+
 public class PerfTest
 {
+    private static Random random = new Random();
     private readonly ITestOutputHelper output;
+
     public PerfTest(ITestOutputHelper output)
     {
         this.output = output;
@@ -31,45 +35,15 @@ public class PerfTest
             => must.BePositive();
     }
 
-    [Fact]
-    public void StringIsValid()
-    {
-        var value = ValueType.TryCreate<FiveCharacterString, string>("valid", out var error);
-        error.HasErrors().Should().BeFalse();
-        value?.Value.Should().Be("valid");
-    }
-
-    [Fact]
-    public void Performance()
-    {
-        FiveCharacterStringCreate(GetRandom5LetterString);
-        FiveCharacterStringCreate(GetRandom5LetterString);
-        // Performance Tests
-
-        ///*Time taken to create 3276700 instances of PositiveInt: 661 ms
-        //Memory used to create instances: 107 kilobytes*/
-        //var getInt = GetRandomPositiveInt(new Random());
-        // Get initial memory usage
-        long initialMemory, finalMemory;
-        initialMemory = GC.GetTotalMemory(true);
-
-        var l1 = PerformanceTest<FiveCharacterString>(short.MaxValue * 100, () => FiveCharacterStringCreate(GetRandom5LetterString), output);
-        // Get final memory usage
-        finalMemory = GC.GetTotalMemory(true);
-        output.WriteLine($"Memory used to create instances: {(finalMemory - initialMemory) / 1024 / 1024} kilobytes");
-
-    }
-
-
-
-
-    //public static PositiveInt PositiveIntCreate(Func<int> getInt) => new(getInt());
-    //public static PositiveInt GenericPositiveIntCreate(Func<int> getInt) => (PositiveInt)TestValueTypeBase.GetValueTypeInstance(typeof(PositiveInt), getInt());
-
     public static FiveCharacterString FiveCharacterStringCreate(Func<string> rand) => ValueType.Create<FiveCharacterString, string>(rand());
-    //public static Email EmailCreate() => new(GetRandomEmail());
-    //public static Email GenericEmailCreate() => (Email)TestValueTypeBase.GetValueTypeInstance(typeof(Email), GetRandomEmail());
 
+    public static string GetRandom5LetterString()
+    {
+        var str = random.Next(10000, 99999).ToString();
+        return $"{str}";
+    }
+
+    public static Func<int> GetRandomPositiveInt(Random random) => () => random.Next(1, short.MaxValue);
 
     public static List<T> PerformanceTest<T>(int iterations, Func<object> action, ITestOutputHelper output)
     {
@@ -87,11 +61,59 @@ public class PerfTest
         return values;
     }
 
-    public static string GetRandom5LetterString()
+    [Fact]
+    public void Performance()
     {
-        var str = Guid.NewGuid().ToString()[..5];
-        return $"{str}";
+        FiveCharacterStringCreate(GetRandom5LetterString);
+        FiveCharacterStringCreate(GetRandom5LetterString);
+        // Performance Tests
+
+        // Get initial memory usage
+        long initialMemory, finalMemory;
+        initialMemory = GC.GetTotalMemory(true);
+
+        var l1 = PerformanceTest<FiveCharacterString>(short.MaxValue * 100, () => FiveCharacterStringCreate(GetRandom5LetterString), output);
+        // Get final memory usage
+        finalMemory = GC.GetTotalMemory(true);
+        output.WriteLine($"Memory used to create instances: {(finalMemory - initialMemory) / 1024 / 1024} kilobytes");
     }
 
-    public static Func<int> GetRandomPositiveInt(Random random) => () => random.Next(1, short.MaxValue);
+    [Fact]
+    public void ProofOfConcept()
+    {
+        const int iterations = short.MaxValue * 1000;
+        var stopwatch = new Stopwatch();
+
+        // Measure time for "a" + "b" + "c"
+        stopwatch.Start();
+        for (int i = 0; i < iterations; i++)
+        {
+            var str = GetRandom5LetterString() + GetRandom5LetterString() + GetRandom5LetterString();
+        }
+        stopwatch.Stop();
+        var concatenationTime = stopwatch.ElapsedMilliseconds;
+        output.WriteLine($"Time taken for concatenation using + operator: {concatenationTime} ms");
+
+        // Measure time for $"{"a"}{"b"}{"c"}"
+        stopwatch.Restart();
+        for (int i = 0; i < iterations; i++)
+        {
+            var str = $"{GetRandom5LetterString()}{GetRandom5LetterString()}{GetRandom5LetterString()}";
+        }
+        stopwatch.Stop();
+        var interpolationTime = stopwatch.ElapsedMilliseconds;
+        output.WriteLine($"Time taken for concatenation using string interpolation: {interpolationTime} ms");
+
+        // Output the results
+        output.WriteLine($"interpolationTime - concatenationTime < 0:{interpolationTime - concatenationTime < 0}");
+        output.WriteLine($"Difference in time: {interpolationTime - concatenationTime} ms");
+    }
+
+    [Fact]
+    public void StringIsValid()
+    {
+        var value = ValueType.TryCreate<FiveCharacterString, string>("valid", out var error);
+        error.HasErrors().Should().BeFalse();
+        value?.Value.Should().Be("valid");
+    }
 }
