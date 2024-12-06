f3ae770b41746be43745bd6ac95c85684ee5548e
Daniel Dengler <daniel@busitec.de>
2024-12-06 07:49:50 +0100

    Add Harmony integration and new benchmarks
    Updated JLib.Tools.Benchmarks.csproj to include Lib.Harmony 2.3.3.
    Refactored PerfTest.cs with new using directives and namespace.
    Enhanced CheckPerf with new equality and inequality benchmarks.
    Expanded StringPerformanceBenchmarks with concatenation and interpolation benchmarks.
    Introduced Swapper class for experimental patching using Harmony.

--- Tools/JLib.Tools.Benchmarks/PerfTest.cs
+++ Tools/JLib.Tools.Benchmarks/PerfTest.cs
@@ -1,21 +1,25 @@
 ﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Diagnostics.Windows.Configs;
-using BenchmarkDotNet.Jobs;
-using BenchmarkDotNet.Loggers;
-using BenchmarkDotNet.Running;
 
+using HarmonyLib;
 
 using JLib.Exceptions;
+using JLib.ValueTypes;
 
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Reflection;
 
-using System.Diagnostics;
-
-
-namespace JLib.ValueTypes.Tests;
+namespace JLib.Tools.Benchmarks;
 
 public class PerfTest
 {
+    private static readonly Func<string> _getRandom5LetterString = GetNextString;
+
+    //[Config(typeof(Config))]
+    private static readonly List<string> _strings = Enumerable.Range(0, 5000).Select(_ => GetRandom5LetterString()).ToList();
+
+    private static IEnumerator<string> _stringEnumerator = _strings.GetEnumerator();
     private static Random random = new Random();
 
     public record FiveCharacterString(string Value) : StringValueType(Value)
@@ -32,7 +36,7 @@ public class PerfTest
             => must.BePositive();
     }
 
-    public static FiveCharacterString FiveCharacterStringCreate(Func<string> rand) => ValueType.Create<FiveCharacterString, string>(rand());
+    public static FiveCharacterString FiveCharacterStringCreate(Func<string> rand) => ValueTypes.ValueType.Create<FiveCharacterString, string>(rand());
 
     public static string GetRandom5LetterString()
     {
@@ -42,38 +46,50 @@ public class PerfTest
 
     public static Func<int> GetRandomPositiveInt(Random random) => () => random.Next(1, short.MaxValue);
 
-    public static List<T> PerformanceTest<T>(int iterations, Func<object> action)
+    private static string GetNextString()
     {
-        var values = new List<T>();
-        var stopwatch = new Stopwatch();
-        stopwatch.Start();
-
-        for (var i = 0; i < iterations; i++)
+        if (!_stringEnumerator.MoveNext())
         {
-            values.Add((T)action());
+            _stringEnumerator = _strings.GetEnumerator();
+            _stringEnumerator.MoveNext();
         }
-
-        stopwatch.Stop();
-        return values;
+        return _stringEnumerator.Current;
     }
 
-    [InProcess]
+    public class CheckPerf
+    {
+        [Params(5, 0)]
+        public int val { get; set; } = 5;
+
+        [Benchmark]
+        public bool[] EqualityCheck()
+        {
+            return Enumerable.Range(0, 1000).Select((x) => val == x).ToArray();
+        }
+
+        [Benchmark]
+        public bool[] InEqualityCheck()
+        {
+            return Enumerable.Range(0, 1000).Select(x => val > x).ToArray();
+        }
+    }
+
+    public class Config : ManualConfig
+    {
+        public Config()
+        {
+            WithOptions(ConfigOptions.DisableOptimizationsValidator);
+        }
+    }
+
     public class StringPerformanceBenchmarks
     {
-        private static readonly List<string> _strings = Enumerable.Range(0, 5000).Select(_ => GetRandom5LetterString()).ToList();
-        private static readonly Func<string> _getRandom5LetterString = GetNextString;
-
-        private static IEnumerator<string> _stringEnumerator = _strings.GetEnumerator();
-
-        private static string GetNextString()
+        [Benchmark]
+        public string ConcatenationOperator()
         {
-            if (!_stringEnumerator.MoveNext())
-            {
-                _stringEnumerator = _strings.GetEnumerator();
-                _stringEnumerator.MoveNext();
-            }
-            return _stringEnumerator.Current;
+            return _getRandom5LetterString() + _getRandom5LetterString() + _getRandom5LetterString() + _getRandom5LetterString() + _getRandom5LetterString();
         }
+
         [GlobalSetup]
         public void GlobalSetup()
         {
@@ -81,23 +97,63 @@ public class PerfTest
         }
 
         [Benchmark]
-        public string FiveCharacterString()
-        {
-            return FiveCharacterStringCreate(_getRandom5LetterString).Value;
-
-        }
-
-        [Benchmark]
-        public string ConcatenationOperator()
-        {
-            return _getRandom5LetterString() + _getRandom5LetterString() + _getRandom5LetterString() + _getRandom5LetterString() + _getRandom5LetterString();
-        }
-
-        [Benchmark]
         public string StringInterpolation()
         {
             return $"{_getRandom5LetterString()}{_getRandom5LetterString()}{_getRandom5LetterString()}{_getRandom5LetterString()}{_getRandom5LetterString()}";
         }
+    }
 
+    // Crimes against god here
+    public class Swapper
+    {
+        // Create a Harmony instance
+        private static Harmony harmony = new Harmony("com.example.patch");
+
+        // Get the method to patch
+        private static MethodInfo originalMethod = typeof(ExceptionBuilder)!.GetMethod("GetException")!;
+
+        [Params("patch", "lockedpatch", "unpatched")]
+        public string? PatchState { get; set; }
+
+        [Benchmark]
+        public string FiveCharacterString()
+        {
+            return FiveCharacterStringCreate(_getRandom5LetterString).Value;
+        }
+
+        [GlobalSetup(Target = nameof(FiveCharacterString))]
+        public void GlobalSetup2()
+        {
+            harmony.Unpatch(originalMethod, HarmonyPatchType.All, "com.example.patch");
+            if (PatchState == "patch")
+            {
+                harmony.Patch(originalMethod, new HarmonyMethod(typeof(PatchClass).GetMethod(nameof(PatchClass.Prefix))!));
+            }
+            if (PatchState == "lockedpatch")
+            {
+                harmony.Patch(originalMethod, new HarmonyMethod(typeof(PatchClass).GetMethod(nameof(PatchClass.LockedPrefix))!));
+            }
+        }
+
+        // #region Experimental Patching
+        public class PatchClass
+        {
+            public static bool LockedPrefix(List<Exception> ____exceptions, List<IExceptionProvider> ____children, object ____exceptionLock, object ____childrenLock)
+            {
+                lock (____exceptionLock)
+                    lock (____childrenLock)
+                        return Prefix(____exceptions, ____children);
+            }
+
+            public static bool Prefix(List<Exception> ____exceptions, List<IExceptionProvider> ____children)
+            {
+                bool condition = ____exceptions.Count == 0 && ____children.Count == 0;
+                if (condition)
+                {
+                    return false; // Returning false skips the original method
+                }
+                return true; // Returning true allows the original method to execute
+            }
+        }
     }
 }
